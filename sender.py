6a1ad3f13c7b70b00dcd56924f74d1ffbe519e8e
boruc07 <33493901+boruc07@users.noreply.github.com>
2020-02-14 11:43:47 +0100

    dupa2 - 1 web socket

--- sender.py
+++ sender.py
@@ -1,60 +1,45 @@
 import socket
 import os
 
+conn = socket.socket()
+host = "127.0.0.1"
+port = 8080
+conn.connect((host,port))
+print("Connected on port {}".format(port))
 
-while 1:
+#sending source image
+filename = "D:\Dokumenty/testy/serwer/flames.png"
+size = os.path.getsize(filename)
+print (size)
+file = open(filename , 'rb')
+file_data = file.read(size)
+conn.send(file_data)
+print("Source image has been transmitted successfully")
 
-    s = socket.socket()
-    host = "192.168.56.1"
-    port = 8080
-    s.bind((host,port))
-    s.listen(1)
+#sending pattern image
+filename = "D:\Dokumenty/testy/serwer/pt.png"
+size = os.path.getsize(filename)
+print (size)
+file = open(filename , 'rb')
+file_data = file.read(size)
+conn.send(file_data)
+print("Pattern image has been transmitted successfully")
 
-    print(host)
-    print("Waiting for any incoming connections ... ")
-    conn, addr = s.accept()
-    print(addr, "Has connected to the server")
- 
-    filename = "D:\Dokumenty/testy/serwer/flames.png"
-    size = os.path.getsize(filename)
-    #print (size)
-    file = open(filename , 'rb')
-    file_data = file.read(size)
-    conn.send(file_data)
-    print("Source image has been transmitted successfully")
-
-    filename = "D:\Dokumenty/testy/serwer/pt.png"
-    size = os.path.getsize(filename)
-   # print (size)
-    file = open(filename , 'rb')
-    file_data = file.read(size)
-    conn.send(file_data)
-    print("Pattern image has been transmitted successfully")
-
-    
-
-
-    sc = socket.socket()
-    host = "192.168.56.1"
-    port = 8081
-    
-    sc.connect((host,port))
-    #s._check_sendfile_params
-    print("Connected on port {}".format(port))
-    file_data = sc.recv(1000000)
+#Progress status
+file_data = conn.recv(1000000)
+file_data = file_data.decode()
+i = 0    
+while file_data != "100":        
+    print("{}% Progress".format(file_data))
+    file_data = conn.recv(100000)
     file_data = file_data.decode()
-    i = 0    
-    while file_data != "100":        
-        print("{}% Progress".format(file_data))
-        file_data = sc.recv(1000000)
-        file_data = file_data.decode()
-        i = i+1
-    print("100% Progress")
+    i = i+1
+print("100% Progress")
 
-    filename = "D:\Dokumenty/testy/serwer/rev.png"
-
-    file = open(filename, 'wb')
-    file_data = sc.recv(1000000)
-    file.write(file_data)
-    file.close()
-    print("Final image has been received successfully.")
+#Reciving final image
+filename = "D:\Dokumenty/testy/serwer/rev.png"
+file = open(filename, 'wb')
+file_data = conn.recv(1000000)
+file.write(file_data)
+file.close()
+print("Final image has been received successfully.")
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import os
 import cv2
 import numpy as np
+import time
 from matplotlib import pyplot as plt
 
 def rotateImage(image, angle):
@@ -10,74 +11,63 @@ def rotateImage(image, angle):
   result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
   return result
 
-
-s = socket.socket()
-host = "192.168.56.1"
-port = 8080
-s.connect((host,port))
-#s._check_sendfile_params
-print("Connected on port {}".format(port))
-
-filename = "D:\Dokumenty/testy/odbiorca/source.png"
-
-file = open(filename, 'wb')
-file_data = s.recv(100000)
-file.write(file_data)
-file.close()
-print("Source has been received successfully.")
-
-filename = "D:\Dokumenty/testy/odbiorca/pattern.png"
-
-file = open(filename, 'wb')
-file_data = s.recv(100000)
-file.write(file_data)
-file.close()
-print("Pattern has been received successfully.")
-
-s = socket.socket()
-host = "192.168.56.1"
-port = 8081
-s.bind((host,port))
-s.listen(1)
-print("Waiting for any incoming connections ... ")
-conn, addr = s.accept()
-print(addr, "Has connected to the server")
-x = 7
-img_rgb = cv2.imread('source.png')
-for n in range(x):
-    i = (360 / x) * n
-
-   
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread('pattern.png',0)
-    #template = cv2.rotate(template, cv2.ROTATE_90_CLOCKWISE)
-    template = rotateImage(template, i)
-    w, h = template.shape[::-1]
-
-    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.85
-    loc = np.where( res >= threshold)
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
-    cv2.imwrite('res.png',img_rgb)
-    img_rgb = cv2.imread('res.png')
-    txt = "Wykonano {}".format((n+1)/x * 100) 
-    to_send = round((n+1)/x * 100)
-    txt = "{}".format(to_send)
-    conn.send(txt.encode())   
-    #txt = "Wykonano {} %".format(n)
-    print(to_send)
-    
+while 1:
+  sc = socket.socket()
+  host = "127.0.0.1"
+  port = 8080
+  sc.bind((host,port))
+  sc.listen(1)
+  print(host)
+  print("Waiting for any incoming connections ... ")
+  s, addr = sc.accept()
+  print(addr, "Has connected to the server")
 
 
 
+  filename = "D:\Dokumenty/testy/odbiorca/source.png"
 
+  file = open(filename, 'wb')
+  file_data = s.recv(100000)
+  file.write(file_data)
+  file.close()
+  print("Source has been received successfully.")
 
+  filename = "D:\Dokumenty/testy/odbiorca/pattern.png"
 
-filename = "D:\Dokumenty/testy/odbiorca/res.png"
-size = os.path.getsize(filename)
-file = open(filename , 'rb')
-file_data = file.read(size)
-#print(size)
-conn.send(file_data)
-print("Final image has been transmitted successfully")
+  file = open(filename, 'wb')
+  file_data = s.recv(100000)
+  file.write(file_data)
+  file.close()
+  print("Pattern has been received successfully.")
+
+  #Calculating best pattern equivalents in source image for each rotation
+  x = 7
+  img_rgb = cv2.imread("D:\Dokumenty/testy/odbiorca/source.png")
+  for n in range(x):
+      i = (360 / x) * n  
+      img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+      template = cv2.imread("D:\Dokumenty/testy/odbiorca/pattern.png",0)
+      template = rotateImage(template, i)
+      w, h = template.shape[::-1]
+
+      res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+      threshold = 0.85
+      loc = np.where( res >= threshold)
+      for pt in zip(*loc[::-1]):
+          cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
+      cv2.imwrite("D:\Dokumenty/testy/odbiorca/res.png",img_rgb)
+      img_rgb = cv2.imread("D:\Dokumenty/testy/odbiorca/res.png")
+      txt = "Wykonano {}".format((n+1)/x * 100) 
+      to_send = round((n+1)/x * 100)
+      txt = "{}".format(to_send)
+      s.send(txt.encode())
+      print(to_send)   
+
+#Sending final image
+  filename = "D:\Dokumenty/testy/odbiorca/res.png"
+  size = os.path.getsize(filename)
+  file = open(filename , 'rb')
+  file_data = file.read(size)
+  print(size)
+  s.send(file_data)
+  print("Final image has been transmitted successfully")
